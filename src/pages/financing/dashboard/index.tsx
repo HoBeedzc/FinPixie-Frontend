1ad37a3ccc8f92194c01c9675847847b4da4a986
hobee <hobee.liu@gmail.com>
2023-11-22 00:39:47 +0800

    Refactor financing dashboard component

--- src/pages/financing/dashboard/index.tsx
+++ src/pages/financing/dashboard/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Grid, Space } from '@arco-design/web-react';
+import Overview from './overview';
+
+const { Row, Col } = Grid;
+
+const gutter = 16;
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Space size={16} align="start">
+      <Space size={16} direction="vertical">
+        <Overview />
+        <Row gutter={gutter}>
+          <Col span={12}>
+            <Overview />
+          </Col>
+          <Col span={12}>
+            <Overview />
+          </Col>
+        </Row>
+      </Space>
+    </Space>
+  );
 }
 
 export default Dashboard;
---
+++ src/pages/financing/dashboard/locale/index.ts
@@ -0,0 +1,22 @@
+const i18n = {
+  'en-US': {
+    'financing.header': 'Hello',
+    'financing.totalAmount': 'Total Financing',
+    'financing.quantity': 'Quantity',
+    'financing.latestRevenue': 'Latest Revenue',
+    'financing.rateOfReturn': 'Annualized rate of return',
+    'financing.pecs': 'CNY',
+    'financing.seeMore': 'See More',
+  },
+  'zh-CN': {
+    'financing.header': '你好',
+    'financing.totalAmount': '总理财',
+    'financing.quantity': '理财数量',
+    'financing.latestRevenue': '最新收益',
+    'financing.rateOfReturn': '年化收益率',
+    'financing.pecs': '人民币',
+    'financing.seeMore': '查看更多',
+  },
+};
+
+export default i18n;
